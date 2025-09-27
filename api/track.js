2f990a945bf52294b20486b52528958d69870531
Ay Technic Company © <aytechnic.corp@gmail.com>
2025-09-27 11:38:11 +0330

    Update track.js

--- api/track.js
+++ api/track.js
@@ -6,8 +6,8 @@ const GOOGLE_SERVICE_ACCOUNT_EMAIL = process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL;
 const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY;
 
 const planToSheetMap = {
-    '12000': '30D', '220000': '60D', '340000': '90D',
-    '600000': '180D', '10000': '365D', '2000000': '730D',
+    '120000': '30D', '220000': '60D', '340000': '90D',
+    '600000': '180D', '1000000': '365D', '2000000': '730D',
 };
 
 module.exports = async (req, res) => {
@@ -31,23 +31,24 @@ module.exports = async (req, res) => {
             const sheet = doc.sheetsByTitle[sheetTitle];
             if (sheet) {
                 const rows = await sheet.getRows();
-                const foundRow = rows.find(row => row.get('trackingId') === trackingId);
-                if (foundRow) {
-                    purchases.push({
-                        plan: sheetTitle,
-                        date: foundRow.get('purchaseDate'),
-                        link: foundRow.get('link'),
-                        name: foundRow.get('name'),
-                        email: foundRow.get('email'),
-                        phone: foundRow.get('phone'),
-                        trackingId: foundRow.get('trackingId')
-                    });
-                }
+                rows.forEach(row => {
+                    if (row.get('trackingId') === trackingId) {
+                        purchases.push({
+                            plan: sheetTitle,
+                            date: row.get('purchaseDate'),
+                            link: row.get('link'),
+                            name: row.get('name'),
+                            email: row.get('email'),
+                            phone: row.get('phone'),
+                            trackingId: row.get('trackingId')
+                        });
+                    }
+                });
             }
         }
         
         if (purchases.length > 0) {
-            return res.status(200).json(purchases);
+            return res.status(200).json(purchases.sort((a, b) => new Date(b.date) - new Date(a.date)));
         } else {
             return res.status(404).json({ error: 'No purchases found with this Tracking ID.' });
         }
